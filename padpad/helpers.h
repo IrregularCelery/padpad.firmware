248d0c12aa2f6b54a9fe7152fed7be4026bd231e
Mohsen Rad <irregular.celery@gmail.com>
2024-12-12 20:53:38 +0330

    Create `RotaryEncoder` helper funtions

--- padpad/helpers.h
+++ padpad/helpers.h
@@ -12,7 +12,7 @@ char* generateKeymap(byte row_count, byte col_count) {
 }
 
 // Function to smooth out analog reading and mapping it to a 100 step value (0-99)
-int analogStepRead(int pin, int* last_step) {
+uint8_t analogStepRead(int pin, int* last_step) {
   const float resolution = 1024;
   const float steps_count = 100;
   const float tolerance = 16;
@@ -54,5 +54,67 @@ void selectChannel(int channel) {
   for (int i = 0; i < 3 /* 3 bits = 8 channels */; i++) {
     digitalWrite(selector_pins[i], bitRead(channel, i));
   }
+}
+#endif
+
+#if !ROTARY_ENCODER_DISABLED
+bool _rotary_encoder_moved = false;
+
+// If you're using an ESP board, this will be needed for the intrupt attached function
+#ifndef IRAM_ATTR
+#define IRAM_ATTR
+#endif
+
+void IRAM_ATTR rotaryEncoderMove() {
+  // Rotary encoder position change detected by intrupt pins
+  _rotary_encoder_moved = true;
+}
+
+bool rotaryEncoderMoved() {
+  return _rotary_encoder_moved;
+}
+
+// Algorithm and docs : https://www.pinteric.com/rotary.html
+int8_t rotaryEncoderRead() {
+  // Reset the position change
+  _rotary_encoder_moved = false;
+
+  static uint8_t lrmem = 3;
+  static int lrsum = 0;
+  static int8_t TRANS[] = { 0, -1, 1, 14, 1, 0, 14, -1, -1, 14, 0, 1, 14, 1, -1, 0 };
+
+  // Read both pin states for rotation detection
+  int8_t l = digitalRead(ROTARY_ENCODER_PIN1);
+  int8_t r = digitalRead(ROTARY_ENCODER_PIN2);
+
+  // Move previous value 2 bits to the left and add in our new values
+  lrmem = ((lrmem & 0x03) << 2) + 2 * l + r;
+
+  // Convert the bit pattern to a movement indicator (14 = impossible, ie switch bounce)
+  lrsum += TRANS[lrmem];
+
+  // Rotary encoder not in the neutral (detent) state
+  if (lrsum % 4 != 0) {
+    return 0;
+  }
+
+  // Rotary encoder in the neutral state - clockwise rotation
+  if (lrsum == 4) {
+    lrsum = 0;
+
+    return 1;
+  }
+
+  // Rotary encoder in the neutral state - counterclockwise rotation
+  if (lrsum == -4) {
+    lrsum = 0;
+
+    return -1;
+  }
+
+  // An impossible rotation has been detected - ignore the movement
+  lrsum = 0;
+
+  return 0;
 }
 #endif
