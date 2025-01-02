f4ab09d571f1beb73ca00c701ad8b21cf37d228e
Mohsen Rad <irregular.celery@gmail.com>
2025-01-02 23:09:02 +0330

    Create `RGBController` class in `lib.h`

--- padpad/lib.h
+++ padpad/lib.h
@@ -78,6 +78,22 @@ public:
   }
 
 private:
+  uint8_t pin_;
+  unsigned long debounce_timeout_;
+  unsigned long double_click_timeout_;
+  unsigned long last_debounce_time_ = 0;
+  unsigned long last_press_time_ = 0;
+
+  bool last_state_;
+  bool state_;
+
+  bool ready_for_double_click_ = false;
+  uint8_t click_count_ = 0;
+
+  bool pressed_detected_ = false;
+  bool release_detected_ = false;
+  bool double_click_detected_ = false;
+
   void handlePress(unsigned long current_time) {
     if (ready_for_double_click_) {
       click_count_++;
@@ -94,22 +110,6 @@ private:
       last_press_time_ = current_time;
     }
   }
-
-  uint8_t pin_;
-  unsigned long debounce_timeout_;
-  unsigned long double_click_timeout_;
-  unsigned long last_debounce_time_ = 0;
-  unsigned long last_press_time_ = 0;
-
-  bool last_state_;
-  bool state_;
-
-  bool ready_for_double_click_ = false;
-  uint8_t click_count_ = 0;
-
-  bool pressed_detected_ = false;
-  bool release_detected_ = false;
-  bool double_click_detected_ = false;
 };
 
 class DynamicRef {
@@ -124,50 +124,50 @@ public:
   DynamicRef() = default;
 
   void pointTo(bool& value) {
-    _value = &value;
-    _type = Bool;
+    value_ = &value;
+    type_ = Bool;
   }
   void pointTo(int& value) {
-    _value = &value;
-    _type = Int;
+    value_ = &value;
+    type_ = Int;
   }
   void pointTo(float& value) {
-    _value = &value;
-    _type = Float;
+    value_ = &value;
+    type_ = Float;
   }
 
   void reset() {
-    _value = nullptr;
-    _type = None;
+    value_ = nullptr;
+    type_ = None;
   }
 
   VariableType getType() {
-    return _type;
+    return type_;
   }
 
   // Bool
   void operator=(bool value) {
-    if (_type == Bool) {
-      *static_cast<bool*>(_value) = value;
+    if (type_ == Bool) {
+      *static_cast<bool*>(value_) = value;
     }
   }
   bool operator==(bool value) const {
-    if (_type == Bool) {
-      return *static_cast<bool*>(_value) == value;
+    if (type_ == Bool) {
+      return *static_cast<bool*>(value_) == value;
     }
 
     return false;
   }
   bool operator!() const {
-    if (_type == Bool) {
-      return !(*static_cast<bool*>(_value));
+    if (type_ == Bool) {
+      return !(*static_cast<bool*>(value_));
     }
 
     return false;  // Return false for non-bool types
   }
   operator bool() const {
-    if (_type == Bool) {
-      return *static_cast<bool*>(_value);
+    if (type_ == Bool) {
+      return *static_cast<bool*>(value_);
     }
 
     return false;
@@ -175,33 +175,33 @@ public:
 
   // Int
   void operator=(int value) {
-    if (_type == Int) {
-      *static_cast<int*>(_value) = value;
+    if (type_ == Int) {
+      *static_cast<int*>(value_) = value;
     }
   }
   bool operator==(int value) const {
-    if (_type == Int) {
-      return *static_cast<int*>(_value) == value;
+    if (type_ == Int) {
+      return *static_cast<int*>(value_) == value;
     }
 
     return false;
   }
   operator int() const {
-    if (_type == Int) {
-      return *static_cast<int*>(_value);
+    if (type_ == Int) {
+      return *static_cast<int*>(value_);
     }
 
     return 0;
   }
   void operator+=(int value) {
-    switch (_type) {
+    switch (type_) {
       case Int:
-        *static_cast<int*>(_value) += value;
+        *static_cast<int*>(value_) += value;
 
         break;
 
       case Float:
-        *static_cast<float*>(_value) += value;
+        *static_cast<float*>(value_) += value;
 
         break;
 
@@ -210,14 +210,14 @@ public:
     }
   }
   void operator-=(int value) {
-    switch (_type) {
+    switch (type_) {
       case Int:
-        *static_cast<int*>(_value) -= value;
+        *static_cast<int*>(value_) -= value;
 
         break;
 
       case Float:
-        *static_cast<float*>(_value) -= value;
+        *static_cast<float*>(value_) -= value;
 
         break;
 
@@ -228,33 +228,33 @@ public:
 
   // Float
   void operator=(float value) {
-    if (_type == Float) {
-      *static_cast<float*>(_value) = value;
+    if (type_ == Float) {
+      *static_cast<float*>(value_) = value;
     }
   }
   bool operator==(float value) const {
-    if (_type == Float) {
-      return *static_cast<float*>(_value) == value;
+    if (type_ == Float) {
+      return *static_cast<float*>(value_) == value;
     }
 
     return false;
   }
   operator float() const {
-    if (_type == Float) {
-      return *static_cast<float*>(_value);
+    if (type_ == Float) {
+      return *static_cast<float*>(value_);
     }
 
     return 0.0f;
   }
   void operator+=(float value) {
-    switch (_type) {
+    switch (type_) {
       case Float:
-        *static_cast<float*>(_value) += value;
+        *static_cast<float*>(value_) += value;
 
         break;
 
       case Int:
-        *static_cast<int*>(_value) += (int)value;
+        *static_cast<int*>(value_) += (int)value;
 
         break;
 
@@ -263,14 +263,14 @@ public:
     }
   }
   void operator-=(float value) {
-    switch (_type) {
+    switch (type_) {
       case Float:
-        *static_cast<float*>(_value) -= value;
+        *static_cast<float*>(value_) -= value;
 
         break;
 
       case Int:
-        *static_cast<int*>(_value) -= (int)value;
+        *static_cast<int*>(value_) -= (int)value;
 
         break;
 
@@ -280,6 +280,222 @@ public:
   }
 
 private:
-  void* _value = nullptr;
-  VariableType _type = None;
+  void* value_ = nullptr;
+  VariableType type_ = None;
+};
+
+struct Color {
+  uint8_t r, g, b;
+
+  Color(uint8_t red, uint8_t green, uint8_t blue) {
+    r = red;
+    g = green;
+    b = blue;
+  }
+
+  Color() {
+    r = 0;
+    g = 0;
+    b = 0;
+  }
+};
+
+class RGBController {
+public:
+  RGBController(unsigned long transition_duration = 1000 /* ms */) {
+    start_color_ = RED;
+    end_color_ = BLUE;
+    duration_ = transition_duration;
+    start_time_ = millis();
+
+    overridden_ = false;
+    transitioning_from_override_ = false;
+    transitioning_to_override_ = false;
+    override_duration_ = 0;
+    override_timeout_ = 0;
+
+    flashing_ = false;
+  }
+
+  // Parameter `delay` means the delay between each flashing (in ms)
+  void flash(int count, Color color, unsigned long delay) {
+    flash_color_ = color;
+    current_flash_start_ = getCurrentColor();
+    current_flash_end_ = flash_color_;
+
+    flashing_ = true;
+
+    flash_count_ = count * 2;  // Fading in and out
+    current_flash_ = 0;
+
+    single_flash_duration_ = delay;
+    flash_start_time_ = millis();
+  }
+
+  // Color of RGBController will be overridden until `timeout` runs out
+  // `timeout` = 0 overrides indefinitely (in ms)
+  void override(Color color, unsigned long timeout = 0, unsigned long transition_duration = 250) {
+    Color current_color = getCurrentColor();
+    start_color_ = current_color;
+    override_color_ = color;
+
+    transitioning_to_override_ = true;
+
+    override_start_time_ = millis();
+    override_duration_ = transition_duration;
+    override_timeout_ = timeout;
+    start_time_ = millis();
+  }
+
+  void override() {
+    // We set it to 1 because it's the smallest number that will resume the transition
+    if (overridden_) override_timeout_ = 1;
+  }
+
+  Color tick() {
+    unsigned long current_time = millis();
+
+    if (flashing_) {
+
+      float flash_progress = (float)(current_time - flash_start_time_) / single_flash_duration_;
+
+      if (flash_progress >= 1.0f) {
+        current_flash_++;
+        flash_start_time_ = current_time;
+
+        if (current_flash_ >= flash_count_) {
+          flashing_ = false;
+
+          return tick();
+        }
+
+        // Swap start and end colors for next fade
+        Color temp = current_flash_start_;
+        current_flash_start_ = current_flash_end_;
+        current_flash_end_ = temp;
+        flash_progress = 0;
+      }
+
+      return Color(
+        current_flash_start_.r + ((current_flash_end_.r - current_flash_start_.r) * flash_progress),
+        current_flash_start_.g + ((current_flash_end_.g - current_flash_start_.g) * flash_progress),
+        current_flash_start_.b + ((current_flash_end_.b - current_flash_start_.b) * flash_progress));
+    }
+
+    if (transitioning_to_override_) {
+      float progress = min(1.0f, (float)(current_time - start_time_) / override_duration_);
+
+      if (progress >= 1.0f) {
+        transitioning_from_override_ = false;
+        overridden_ = true;
+      }
+
+      return Color(
+        start_color_.r + (override_color_.r - start_color_.r) * progress,
+        start_color_.g + (override_color_.g - start_color_.g) * progress,
+        start_color_.b + (override_color_.b - start_color_.b) * progress);
+    }
+
+    if (overridden_) {
+      if (override_timeout_ > 0 && (current_time - override_start_time_) >= override_timeout_) {
+        overridden_ = false;
+        transitioning_from_override_ = true;
+
+        start_time_ = current_time;
+
+        start_color_ = override_color_;
+        end_color_ = getCurrentColor();
+      } else {
+        return override_color_;
+      }
+    }
+
+    if (transitioning_from_override_) {
+      float progress = min(1.0f, (float)(current_time - start_time_) / override_duration_);
+
+      if (progress >= 1.0f) {
+        transitioning_from_override_ = false;
+        start_time_ = current_time;
+      }
+
+      return Color(
+        start_color_.r + (end_color_.r - start_color_.r) * progress,
+        start_color_.g + (end_color_.g - start_color_.g) * progress,
+        start_color_.b + (end_color_.b - start_color_.b) * progress);
+    }
+
+    float progress = min(1.0f, (float)(current_time - start_time_) / duration_);
+
+    Color current_color = getCurrentColor();
+
+    if (progress >= 1.0f) {
+      nextTransition();
+    }
+
+    return current_color;
+  }
+
+private:
+  // ---------- Normal ---------- //
+
+  Color start_color_;
+  Color end_color_;
+  Color last_color_;
+
+  unsigned long start_time_;
+  unsigned long duration_;
+
+  // ---------- Override ---------- //
+
+  Color override_color_;
+
+  bool overridden_;
+  bool transitioning_from_override_;
+  bool transitioning_to_override_;
+
+  unsigned long override_start_time_;
+  unsigned long override_duration_;
+  unsigned long override_timeout_;
+
+  // ---------- Flash ---------- //
+
+  Color flash_color_;
+  Color current_flash_start_;
+  Color current_flash_end_;
+
+  bool flashing_;
+
+  int flash_count_;
+  int current_flash_;
+
+  unsigned long flash_start_time_;
+  unsigned long flash_duration_;
+  unsigned long single_flash_duration_;
+
+  const Color RED{ 255, 0, 0 };
+  const Color GREEN{ 0, 255, 0 };
+  const Color BLUE{ 0, 0, 255 };
+
+  Color getCurrentColor() {
+    float progress = min(1.0f, (float)(millis() - start_time_) / duration_);
+
+    return Color(
+      start_color_.r + ((end_color_.r - start_color_.r) * progress),
+      start_color_.g + ((end_color_.g - start_color_.g) * progress),
+      start_color_.b + ((end_color_.b - start_color_.b) * progress));
+  }
+
+  void nextTransition() {
+    start_color_ = end_color_;
+
+    if (end_color_.b == 255) {         // End of color blue
+      end_color_ = GREEN;              // Change color to Green
+    } else if (end_color_.g == 255) {  // End of color green
+      end_color_ = RED;                // Change color to Red
+    } else if (end_color_.r = 255) {   // End of color Red
+      end_color_ = BLUE;               // Change color to Blue
+    }
+
+    start_time_ = millis();
+  }
 };
